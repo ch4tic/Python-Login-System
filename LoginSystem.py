8daff89d91760ae400729b6a8bf3301f53ee2b3c
Eman Ćatić <66844759+ch4tic@users.noreply.github.com>
2020-06-14 19:25:00 +0200

    Add files via upload

--- LoginSystem.py
+++ LoginSystem.py
@@ -9,6 +9,9 @@ import os
 import sys
 import random
 
+global commands
+commands = ['Change username', 'Reset password']
+
 # Function for clearing the screen.
 def clear():
     os.system('cls')
@@ -25,6 +28,7 @@ def intro():
 def folderMake():
     os.system('d:')
     os.system('mkdir credentials2')
+
 # Sign up function.
 def signUp():
     clear()
@@ -45,7 +49,6 @@ def signUp():
         password = input('Password: ')
     # User is prompted for input while the length is less than 8 characters.
     while len(password) < 8:
-        new()
         print('Password is too short! (min. 8 characters)')
         time.sleep(1)
         password = input('Password: ')
@@ -64,39 +67,97 @@ def signUp():
     intro()
     signIn()
 
+def tooManyInvalid():
+    clear()
+    intro()
+    print('Commands: ')
+    for command in commands:
+        print('-' * len(command))
+        print(command, sep='\n')
+        print('-' * len(command))
+        time.sleep(1)
+    commandInput = input('Enter a command: ')
+    while commandInput == '':
+        commandInput = input('Enter a command: ')
+    while commandInput not in commands:
+        print('Command not available!')
+        commandInput = input('Enter a command: ')
+    if commandInput == commands[0]:
+        usernameChange()
+    elif commandInput == commands[1]:
+        passwordReset()
+
+
+
 # Sign in function.
 def signIn():
     clear()
     intro()
+    global usrLogin
+    global pwdLogin
+    global data
+    global file
     time.sleep(2)
-    new()
-    while True:
-        usrLogin = input("Username: ") # User enters his account username.
-        # Program tries to unlock the folder with credentials and open it.
-        try:
-            os.system('cacls credentials2 /P everyone:f')
-            clear()
-            intro()
-            # Opening the folder with the correct path.
-            file = open(r'D:/ch4tic/Programiranje/Python Programs/Login System/credentials2/' + usrLogin + ".txt", "r")
-        # If it is not possible, program prints out that it is incorrect.
-        except:
-            print('Incorrect username!')
-
-        data = file.readline() # Program reads the file.
-        pwdLogin = input('Password: ') # User enters his account password
-        file.close() # Program exits the file after reading it.
-        os.system('cacls credentials2 /P everyone:n') # Program locks the folder and no user can access it.
+    usrLogin = input("Username: ") # User enters his account username.
+    # Program tries to unlock the folder with credentials and open it.
+    os.system('cacls credentials2 /P everyone:f')
+    try:
         clear()
         intro()
-        # If credentials are correct, user logs in!
-        if data == usrLogin + ':' + pwdLogin:
-            print('Welcome!')
+        # Opening the folder with the correct path.
+        file = open('D:/ch4tic/Programiranje/Python Programs/Login System/credentials2/' + usrLogin + ".txt", "r")
+    # If it is not possible, program prints out that it is incorrect.
+    except:
+        print('Incorrect username!')
+    data = file.readline() # Program reads the file.
+    pwdLogin = input('Password: ') # User enters his account password
+    file.close() # Program exits the file after reading it.
+    os.system('cacls credentials2 /P everyone:n') # Program locks the folder and no user can access it.
+    clear()
+    intro()
+    # If credentials are correct, user logs in!
+    if data == usrLogin + ':' + pwdLogin:
+        print('Welcome ' + str(usrLogin) + '!')
+        time.sleep(1)
+        print('Commands: ')
+        for command in commands:
+            print('-' * len(command))
+            print(command, sep='\n')
+            print('-' * len(command))
             time.sleep(1)
-            sys.exit()
-        else:
-            print("Incorrect username or password.")
+        commandInput = input('Enter a command: ')
+        while commandInput == '':
+            commandInput = input('Enter a command: ')
+        while commandInput not in commands:
+            print('Command not available!')
+            commandInput = input('Enter a command: ')
+        if commandInput == commands[0]:
+            usernameChange()
+        elif commandInput == commands[1]:
+            passwordReset()
+    else:
+        print("Incorrect username or password.")
 
+def usernameChange():
+    clear()
+    intro()
+    currentUsername = input('Enter current username: ')
+    while currentUsername == '' or currentUsername != usrLogin:
+        currentUsername = input('Enter current username: ')
+    if currentUsername == usrLogin:
+        print('Username is valid.')
+        newUsername = input('New username: ')
+        while newUsername == '':
+            newUsername = input('New username: ')
+        clear()
+        intro()
+        os.system('cacls credentials2 /P everyone:f')
+        file = open(r'D:/ch4tic/Programiranje/Python Programs/Login System/credentials2/' + newUsername + ".txt", "w")
+        file.write(newUsername + ":" + pwdLogin)
+        file.close()
+        os.system('d:')
+        os.chdir('credentials2')
+        os.system('del ' + usrLogin + '.txt')
 # Function for a user to decide whether he wants to sign up or sign in.
 def choices():
     time.sleep(2) # Time delay.
@@ -110,17 +171,15 @@ def choices():
     else:
         print('No choice specified!')
         time.sleep(1)
-        sys.quit()
+        sys.exit()
 
 # Main function calls all the needed functions.
 def main():
     intro() # Welcomes the user to the program.
     folderMake() # Function that makes the credentials folder.
-    protect() # Function that protects the folder so no one can access it.
     choices() # User has an option to either sign up or sign in.
 
 main() # Calling the main function.
-
 
 # Project done: False.
 # Deadline: 06.20.2020.
