90b031bb31baa7c21c5e683443c8c33b1c76210b
ch4tic <belusic012@gmail.com>
2020-06-16 11:20:35 +0200

    Fixed some bugs and optimized program for Linux

--- LoginSystem.py
+++ LoginSystem.py
@@ -10,11 +10,13 @@ import sys
 import random
 
 global commands
-commands = ['Change username', 'Reset password']
+global path 
+commands = ['Change username', 'Reset password', 'Exit']
+path = os.getcwd() + '/credentials2/'
 
 # Function for clearing the screen.
 def clear():
-    os.system('cls')
+    os.system('clear')
 # Function for a new line.
 def new():
     print('\n')
@@ -25,8 +27,7 @@ def intro():
     print(welcomeString)
     print('-' * len(welcomeString))
 
-def folderMake():
-    os.system('d:')
+def folderMake(): 
     os.system('mkdir credentials2')
 
 # Sign up function.
@@ -54,15 +55,13 @@ def signUp():
         password = input('Password: ')
 
     # Unlocking the credentials folder.
-    os.system('cacls credentials2 /P everyone:f')
+
     clear()
     intro()
     # Opening the folder.
-    file = open(r'D:/ch4tic/Programiranje/Python Programs/Login System/credentials2/' + username + ".txt", "w")
+    file = open(path + username + ".txt", "w")
     file.write(username + ":" + password)
     file.close()
-    # Locking the folder so no one can access it.
-    os.system('cacls credentials2 /P everyone:n')
     clear()
     intro()
     signIn()
@@ -89,6 +88,7 @@ def tooManyInvalid():
 
 
 
+
 # Sign in function.
 def signIn():
     clear()
@@ -100,19 +100,17 @@ def signIn():
     time.sleep(2)
     usrLogin = input("Username: ") # User enters his account username.
     # Program tries to unlock the folder with credentials and open it.
-    os.system('cacls credentials2 /P everyone:f')
     try:
         clear()
         intro()
         # Opening the folder with the correct path.
-        file = open('D:/ch4tic/Programiranje/Python Programs/Login System/credentials2/' + usrLogin + ".txt", "r")
+        file = open(path + usrLogin + ".txt", "r")
     # If it is not possible, program prints out that it is incorrect.
     except:
         print('Incorrect username!')
     data = file.readline() # Program reads the file.
     pwdLogin = input('Password: ') # User enters his account password
     file.close() # Program exits the file after reading it.
-    os.system('cacls credentials2 /P everyone:n') # Program locks the folder and no user can access it.
     clear()
     intro()
     # If credentials are correct, user logs in!
@@ -135,6 +133,12 @@ def signIn():
             usernameChange()
         elif commandInput == commands[1]:
             passwordReset()
+        elif commandInput == commands[2]: 
+            clear() 
+            intro()
+            print('Goodbye my friend!')
+            time.sleep(1)
+            sys.exit()
     else:
         print("Incorrect username or password.")
 
@@ -151,13 +155,11 @@ def usernameChange():
             newUsername = input('New username: ')
         clear()
         intro()
-        os.system('cacls credentials2 /P everyone:f')
-        file = open(r'D:/ch4tic/Programiranje/Python Programs/Login System/credentials2/' + newUsername + ".txt", "w")
+        file = open(path + newUsername + ".txt", "w")
         file.write(newUsername + ":" + pwdLogin)
         file.close()
-        os.system('d:')
         os.chdir('credentials2')
-        os.system('del ' + usrLogin + '.txt')
+        os.system('rm ' + usrLogin + '.txt')
 # Function for a user to decide whether he wants to sign up or sign in.
 def choices():
     time.sleep(2) # Time delay.
@@ -181,5 +183,5 @@ def main():
 
 main() # Calling the main function.
 
-# Project done: False.
+# Project done: Done.
 # Deadline: 06.20.2020.
--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # Python-Login-System
 
-Login system made in Python 3.
-Currently working on adding these features:
-1. Password reset.
-2. Two factor authentication.
-
-Added a username change feature
+Login system made in Python 3. 
+Fixed minor bugs. 
+This program is entirely made for LINUX. 
 
 ----------------------------
 
